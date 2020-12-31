4378cf7168a17ce7fecfd8c23a11057ae532a098
Ryan Hartlage <ryanplusplus@gmail.com>
2020-12-31 14:29:57 -0500

    It's working! It's working!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Adafruit_VL6180X.h>
 
 extern "C" {
 #include <stddef.h>
@@ -8,9 +9,14 @@ extern "C" {
 }
 
 static tiny_timer_group_t timer_group;
+static Adafruit_VL6180X sensor;
 
 void setup()
 {
+  Serial.begin(9600);
+
+  sensor.begin();
+
   tiny_timer_group_init(&timer_group, tiny_time_source_init());
   tiny_heartbeat_init(&timer_group, 1000);
 }
@@ -18,4 +24,17 @@ void setup()
 void loop()
 {
   tiny_timer_group_run(&timer_group);
+
+  Serial.println("Lux: " + String(sensor.readLux(VL6180X_ALS_GAIN_5)));
+
+  uint8_t range = sensor.readRange();
+
+  if(sensor.readRangeStatus() == VL6180X_ERROR_NONE) {
+    Serial.println("Range: " + String(range));
+  }
+  else {
+    Serial.println("Range: ?");
+  }
+
+  delay(500);
 }
